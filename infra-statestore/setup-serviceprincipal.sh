78a80fe21d88067d037f51136fdd9585717c2c44
Wai Liu <waiholiu@users.noreply.github.com>
2024-10-17 02:22:20 +0000

    created my github actions workflow

--- infra-statestore/setup-serviceprincipal.sh
+++ infra-statestore/setup-serviceprincipal.sh
@@ -3,10 +3,10 @@
 
 
 # Define variable for display name
-DISPLAY_NAME="<uniquename>-<branch>-oidc"
+DISPLAY_NAME="workshop-master-oidc"
 # Define variables for name and subject
-FEDERATED_IDENTITY_NAME="<uniquename>-<branch>-federated-identity"
-FEDERATED_IDENTITY_SUBJECT="repo:<account>/<reponame>:ref:refs/heads/<branch>"
+FEDERATED_IDENTITY_NAME="workshop-master-federated-identity"
+FEDERATED_IDENTITY_SUBJECT="repo:waiholiu/GHActionsTerraformWorkshop:ref:refs/heads/master"
 
 
 AZURE_TENANT=$(az account show -o tsv --query tenantId)
